b4b41acf11babf0aa5f71411c3d5f3d2ab23968b
Eddy Díaz <ediaz3535@gmail.com>
2023-09-17 20:37:09 -0600

    Documentacion

--- README.md
+++ README.md
@@ -1,79 +1,65 @@
-# TAREA 1
+# DB_TSE
 
-Cree una BDD, y ejecute las siguientes instrucciones, por cada instrucción realizada tome captura de la respuesta de su asistente (workbench, datagrip, dbeaver, etc) y subir en un solo documento en formato pdf las capturas correspondientes:
+El programa DB_TSE es una implementación de una base de datos para el Tribunal Supremo Electoral (TSE), esta organización se sabe que debe de almacenar muchos datos en el dia de elecciones de Guatemala. Cuando carecemos de un sistema de información en nuestra organización, optamos por cambiar nuestra manera habitual de almacenar nuestros datos de forma física a una forma virtual. En estas situaciones, es necesario que diseñemos una estructura que nos permita guardar la información de manera persistente y de fácil acceso. 
 
-## Instrucción 1
+Se utilizó modelado de bases de datos previo a la implementación del programa para tener un mejor orden y manejo de datos, la base de datos tiene los siguientes modelos:
 
-create table CAMIONES (
+### Modelo Conceputal
+![modelo_concep](src/app/public/res/Modelo%20Conceptual.png)
 
-COD_CAMION INTEGER not null,
+El objetivo principal de un modelo conceptual es proporcionar una vista clara y comprensible de la base de datos que sirvió como base para el diseño y desarrollo posterior del sistema de gestión de bases de datos (DBMS) y la base de datos en sí.
 
-MARCA VARCHAR2(150),
+### Modelo Logico
+![modelo_logic](src/app/public/res/Logical.png)
 
-MODELO VARCHAR2(300),
+El modelo lógico es una representación intermedia que se centra en cómo los datos se almacenaron y organizaron en el sistema de gestión de bases de datos.
 
- ANNO NUMBER,
+### Modelo Relacional
+![modelo_rela](src/app/public/res/Relational_1.png)
 
-PESO_CARGA INTEGER,
+Este modelo se centra en aspectos técnicos y de rendimiento, y su propósito principal es proporcionar una guía precisa para la creación y administración de la base de datos en el entorno de producción
 
- PATENTE VARCHAR2(300) ) ;
+Este modelo cuenta con las siguientes tablas:
+- Ciudadano
 
- 
+    Esta tabla es la que contiene los datos de ciudadanos y tiene como llave primaria el atributo DPI. Esta relacionada de uno a muchos con la tabla voto.
+- Voto
 
-## Instrucción 2
+    Esta tabla es la que surgió de la normalización, contiene los datos de votos y tiene como llave primaria el atributo ID_VOTO. Esta relacionada de uno a muchos con la tabla detalle_voto y con mesa. Las llaves foraneas son DPI (tabla ciudadano) y ID_MESA (tabla mesa)
+- Mesa
 
-alter table CAMIONES add constraint PK_CAMIONES primary key (COD_CAMION);
+    Esta tabla es la que contiene los datos de mesas y tiene como llave primaria el atributo ID_MESA. Las llaves foraneas son ID_DEPAR (tabla departamento)
+- Departamento
 
- 
+    Esta tabla es la que contiene los datos de departamento y tiene como llave primaria el atributo ID_DEPAR. 
+- Detalle Voto
 
-## Instrucción 3
+    Esta tabla es la que surgió de la normalización, contiene los datos de votos y tiene como llave primaria el atributo ID_DETALLE. Esta relacionada de uno a muchos con la tabla detalle_voto y con candidato. Las llaves foraneas son ID_VOTO (tabla voto) y ID_CANDIDATO (tabla candidato)
+- Candidato
 
-drop table CAMIONES EJEMPLOS 2;
+    Esta tabla es la que contiene los datos de candidatos y tiene como llave primaria el atributo ID_CANDIDATO. Las llaves foraneas son ID_CARGO (tabla cargo) y ID_PARTIDO (tabla partido)
+- Partido
 
- 
+    Esta tabla es la que contiene los datos de partido y tiene como llave primaria el atributo ID_PARTIDO. 
+- Cargo
 
-## Instrucción 4
+    Esta tabla es la que contiene los datos de cargo y tiene como llave primaria el atributo ID_CARGO. 
 
-create table CAMIONES (
+**Normalización**
 
-COD_CAMION INTEGER not null,
+Las tablas anteriores muestran normalización, para ello al archivo CSV de votaciones se le aplicó la Primera Forma Normal (1NF) pero no era valida su implementación. La Segunda Forma Normal (2NF) si se podia aplicar al archivo por lo tanto se creo una nueva tabla con llave primaria id_detalle la cual es un atributo autoincremental.
 
-MARCA VARCHAR2(150),
+![modelo_rela](src/app/public/res/Normalizacion.png)
 
-MODELO VARCHAR2(300),
+### Implementación de API
 
-ANNO NUMBER,
+Una API de Nodejs con MySQL es beneficioso debido a su rendimiento, escalabilidad, facilidad de uso y la amplia comunidad y recursos disponibles. Esto lo hace adecuado para una variedad de aplicaciones, desde pequeños proyectos hasta aplicaciones empresariales de alto tráfico.
 
-PESO_CARGA INTEGER,
+El servidor de la aplicación inicia en el puerto **3000** y muestra un mensaje en la consola indicando que el servidor está corriendo en ese puerto. El puerto **3000** es comúnmente utilizado para aplicaciones web, pero puede ser cambiado a otro puerto si es necesario.
 
-PATENTE VARCHAR2(300) );
+La aplicación se compone de 11 consultas las cuales cada una tiene un endpoint y pueden ser vistas en [script.sql](src/app/public/db.sql) ademas existen endpoint para la cración, carga y eliminación del modelo para la base de datos.
 
- 
-
-## Instrucción 5
-
-alter table CAMIONES add constraint PK_CAMIONES primary key (COD_CAMION);
-
- 
-
-## Instrucción 6
-
-insert into CAMIONES (COD_CAMION, MARCA, MODELO, ANNO, PESO_CARGA, PATENTE)
-
-values (1000, 'susuki', 'baleno', 1995, 45, 'pi-1516');
-
- 
-
-## Instrucción 7
-
- 
-
-update camiones c set c.modelo='probando'
-
-where c.cod_camion=1000;
-
- 
-
-## Instrucción 8
-
-delete from camiones c where c.cod_camion=1000;
+- _Universidad de San Carlos de Guatemala_
+- _Facultad de Ingenieria_
+- _Eddy Fernando Díaz Galindo_
+- _201906558_
--- src/app/public/db.sql
+++ src/app/public/db.sql
@@ -1,9 +1,288 @@
-CREATE TABLE CIUDADANOS(
-    dpi INT(13) NOT NULL PRIMARY KEY ,
-    nombre VARCHAR(25) NOT NULL ,
-    apellido VARCHAR(25) NOT NULL ,
-    direccion VARCHAR(50) NOT NULL ,
-    telefono INT NOT NULL ,
-    edad INT NOT NULL ,
-    genero VARCHAR(1) NOT NULL
-);
+--CREACION DE TABLAS Y BASE DE DATOS
+
+CREATE SCHEMA IF NOT EXISTS minitrep
+
+USE minitrep
+
+CREATE TABLE IF NOT EXISTS minitrep.departamento (
+	id_depar INT PRIMARY KEY,
+	nombre VARCHAR(25) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS minitrep.mesa (
+	id_mesa INT PRIMARY KEY,
+	id_depar INT NOT NULL,
+	FOREIGN KEY (id_depar) REFERENCES minitrep.departamento(id_depar)
+)
+
+CREATE TABLE IF NOT EXISTS minitrep.ciudadano (
+	dpi VARCHAR(13) PRIMARY KEY NOT NULL,
+	nombre VARCHAR(50) NOT NULL,
+	apellido VARCHAR(50) NOT NULL,
+	direccion VARCHAR(75) NOT NULL,
+	telefono VARCHAR(10) NOT NULL,
+	edad INT NOT NULL,
+	genero VARCHAR(1) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS minitrep.voto (
+	id_voto INT PRIMARY KEY NOT NULL,
+	fechahora DATETIME NOT NULL,
+	dpi VARCHAR(13) NOT NULL,
+	id_mesa INT NOT NULL,
+	FOREIGN KEY (dpi) REFERENCES minitrep.ciudadano(dpi),
+	FOREIGN KEY (id_mesa) REFERENCES minitrep.mesa(id_mesa)
+)
+
+CREATE TABLE IF NOT EXISTS minitrep.partido (
+	id_partido INT PRIMARY KEY NOT NULL,
+	nombre_partido VARCHAR(50) NOT NULL,
+	siglas VARCHAR(15) NOT NULL,
+	fundacion DATE NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS minitrep.cargo (
+	id_cargo INT PRIMARY KEY NOT NULL,
+	cargo VARCHAR(40) NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS minitrep.candidato (
+	id_candidato INT PRIMARY KEY NOT NULL,
+	nombre VARCHAR(40) NOT NULL,
+	fecha_naci DATE NOT NULL,
+	id_cargo INT NOT NULL,
+	id_partido INT NOT NULL,
+	FOREIGN KEY (id_cargo) REFERENCES minitrep.cargo(id_cargo),
+	FOREIGN KEY (id_partido) REFERENCES minitrep.partido(id_partido)
+)
+
+CREATE TABLE IF NOT EXISTS minitrep.detalle_voto (
+	id_detalle INT AUTO_INCREMENT PRIMARY KEY,
+	id_voto INT NOT NULL,
+	id_candidato INT NOT NULL,
+	FOREIGN KEY (id_voto) REFERENCES minitrep.voto(id_voto),
+	FOREIGN KEY (id_candidato) REFERENCES minitrep.candidato(id_candidato)
+)
+
+
+--TABLAS TEMPORALES
+
+CREATE TEMPORARY TABLE IF NOT EXISTS minitrep.tmpdepartamentos (
+      id INT NOT NULL,
+      nombre VARCHAR(25) NOT NULL
+)
+
+CREATE TEMPORARY TABLE IF NOT EXISTS minitrep.tmpmesas (
+      id_mesa INT NOT NULL,
+      id_departamento INT NOT NULL
+)
+
+CREATE TEMPORARY TABLE IF NOT EXISTS minitrep.tmpciudadanos (
+      DPI VARCHAR(13) NOT NULL,
+      Nombre VARCHAR(50) NOT NULL,
+      Apellido VARCHAR(50) NOT NULL,
+      Direccion VARCHAR(75) NOT NULL,
+      Telefono VARCHAR(10) NOT NULL,
+      Edad INT NOT NULL,
+      Genero VARCHAR(1) NOT NULL
+)
+
+CREATE TEMPORARY TABLE IF NOT EXISTS minitrep.tmpcandidatos (
+      id INT NOT NULL,
+      nombres VARCHAR(40) NOT NULL,
+      fecha_nacimiento DATE NOT NULL,
+      cargo_id INT NOT NULL,
+      partido_id INT NOT NULL
+)
+
+CREATE TEMPORARY TABLE IF NOT EXISTS minitrep.tmpcargos (
+      id INT NOT NULL,
+      cargo VARCHAR(40) NOT NULL
+)
+
+CREATE TEMPORARY TABLE IF NOT EXISTS minitrep.tmppartidos (
+      id_partido INT NOT NULL,
+      nombrePartido VARCHAR(50) NOT NULL,
+      Siglas VARCHAR(15) NOT NULL,
+      Fundacion DATE NOT NULL
+)
+    
+CREATE TEMPORARY TABLE IF NOT EXISTS minitrep.tmpvotaciones (
+      id_voto INT NOT NULL,
+      id_candidato INT NOT NULL,
+      dpi_ciudadano VARCHAR(13) NOT NULL,
+      mesa_id INT NOT NULL,
+      fecha_hora DATETIME NOT NULL
+)
+
+
+--CONSULTA 1
+
+SELECT 
+    P.nombre_partido AS Partido, 
+    CP.nombre AS Presidente, 
+    CV.nombre AS Vicepresidente
+FROM 
+    minitrep.partido P
+LEFT JOIN 
+    minitrep.candidato CP 
+ON CP.id_cargo = 1 AND P.id_partido = CP.id_partido
+LEFT JOIN 
+    minitrep.candidato CV 
+ON CV.id_cargo = 2 AND P.id_partido = CV.id_partido
+WHERE
+    CP.id_candidato IS NOT NULL
+    AND CV.id_candidato IS NOT NULL;
+    
+--CONSULTA 2
+
+SELECT
+    P.nombre_Partido AS Partido,
+    COUNT(C.id_candidato) AS No_Candidatos
+FROM
+    minitrep.partido P
+LEFT JOIN
+    minitrep.candidato C
+ON P.id_partido = C.id_partido
+LEFT JOIN
+    minitrep.cargo CR
+ON C.id_cargo = CR.id_cargo
+WHERE
+    CR.cargo LIKE 'diputado congreso lista nacional'
+    OR CR.cargo LIKE 'diputado congreso distrito electoral'
+    OR CR.cargo LIKE 'diputado parlamento centroamericano'
+GROUP BY
+    P.nombre_Partido;
+
+
+--CONSULTA 3
+
+SELECT 
+    P.nombre_partido AS Partido, 
+    CA.nombre AS Alcalde
+FROM 
+    minitrep.partido P
+LEFT JOIN 
+    minitrep.candidato CA 
+ON CA.id_cargo = 6 AND P.id_partido = CA.id_partido
+WHERE
+    CA.id_candidato IS NOT NULL
+
+
+--CONSULTA 4
+
+SELECT
+            P.nombre_Partido AS Partido,
+            COUNT(C.id_candidato) AS No_Candidatos
+        FROM
+            minitrep.partido P
+        LEFT JOIN
+            minitrep.candidato C
+        ON P.id_partido = C.id_partido 
+        WHERE
+            P.nombre_Partido <> 'NULO'
+        GROUP BY
+            P.nombre_Partido;
+
+--CONSULTA 5
+
+SELECT
+    Dep.nombre AS Departamento,
+    COUNT(V.id_voto) AS No_Votos
+FROM
+    minitrep.departamento Dep
+LEFT JOIN
+    minitrep.mesa M
+ON
+    Dep.id_depar = M.id_depar
+LEFT JOIN
+    minitrep.voto V
+ON
+    M.id_mesa = V.id_mesa
+GROUP BY
+    Dep.nombre;
+
+--CONSULTA 6
+
+SELECT COUNT(DISTINCT id_voto) AS Total_Nulos,
+COUNT(*) AS Nulos_Por_Persona
+FROM minitrep.detalle_voto
+WHERE id_candidato = -1;
+
+--CONSULTA 7
+
+SELECT edad, COUNT(*) AS Cantidad
+FROM minitrep.ciudadano
+GROUP BY edad
+ORDER BY Cantidad DESC
+LIMIT 10;
+
+--CONSULTA 8
+
+SELECT
+    CP.nombre AS Presidente,
+    CV.nombre AS Vicepresidente,
+    SUM(CASE WHEN (V.id_candidato = CP.id_candidato) OR (V.id_candidato = CV.id_candidato) THEN 1 ELSE 0 END) AS Votos
+FROM
+    minitrep.partido P
+LEFT JOIN
+    minitrep.candidato CP
+ON
+    P.id_partido = CP.id_partido
+    AND CP.id_cargo = 1  
+INNER JOIN
+    minitrep.candidato CV
+ON
+    P.id_partido = CV.id_partido
+    AND CV.id_cargo = 2  
+INNER JOIN
+    minitrep.detalle_voto V
+ON
+    CP.id_candidato = V.id_candidato OR CV.id_candidato = V.id_candidato
+GROUP BY
+    P.nombre_partido, CP.id_candidato, CV.id_candidato
+ORDER BY Votos DESC
+LIMIT 10;
+
+--CONSULTA 9
+
+SELECT
+    M.id_mesa AS No_Mesa,
+    D.nombre AS Departamento,
+    COUNT(*) AS Votos
+FROM
+    minitrep.mesa M
+INNER JOIN
+    minitrep.departamento D
+ON
+    M.id_depar = D.id_depar
+INNER JOIN
+    minitrep.voto V
+ON
+    M.id_mesa = V.id_mesa
+GROUP BY
+    M.id_mesa, D.nombre
+ORDER BY
+    Votos DESC
+LIMIT 5;
+
+--CONSULTA 10
+
+SELECT 
+    TIME_FORMAT(TIME(fechahora), '%H:%i') AS Hora,
+    COUNT(fechahora) AS Votantes
+FROM minitrep.voto V
+GROUP BY V.fechahora
+ORDER BY Votantes DESC
+LIMIT 5;
+
+--CONSULTA 11
+
+SELECT 
+    C.genero AS Genero,
+    COUNT(*) AS Total
+FROM minitrep.voto V
+JOIN minitrep.ciudadano C
+ON V.dpi = C.dpi
+GROUP BY
+    C.genero;
